0ed90f687aa4efa01b5576f85aad9c9128014682
Hakari-Bibani <hakary.jalal33@gmail.com>
2024-10-17 00:06:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ class FreezingPointCalculator:
             col1, col2 = st.columns(2)
             with col1:
                 st.image(image1, use_column_width=True)
-                st.caption("م.هەکاری جلال")
+                st.caption("Image 1 Description")
             with col2:
                 st.image(image2, use_column_width=True)
-                st.caption("گروپی تێلێگرام")
+                st.caption("Image 2 Description")
 
         col1, col2, col3 = st.columns(3)
 
@@ -308,9 +308,8 @@ def main():
     # Add a horizontal line for visual separation
     st.markdown("---")
 
-   # Add the footer sentence
-st.markdown(""" <p style='text-align: center; color: gray; font-style: italic;'> بۆ یەکەمین جار ئەم جۆرە بەرنامەیە دروستکراوە و گەشەی پێدراوە لە کوردستان و عێراق دا. هیوادارم سوودی لێوەربگرن.
-م. هەکاری جلال محمد </p> """, unsafe_allow_html=True)
+    # Add the footer sentence
+    st.markdown("<p style='text-align: center; color: gray; font-style: italic;'>دروستکردنی بەرنامە: م.هەکاری جلال</p>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
