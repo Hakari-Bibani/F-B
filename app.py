5702f9bd1f5e90c24d533a37e4501465c929905d
Hakari-Bibani <hakary.jalal33@gmail.com>
2024-10-17 00:01:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -248,9 +248,12 @@ def main():
     image1, image2 = load_images()
 
           # Move the sentence to the top of the app
-    st.markdown(""" <p style='text-align: center; color: gray; font-style: italic;'> بۆ یەکەمین جار ئەم جۆرە بەرنامەیە دروستکراوە و گەشەی پێدراوە لە کوردستان و عێراق دا. هیوادارم سوودی لێوەربگرن </p> """, unsafe_allow_html=True)
+    st.markdown(""" <p style='text-align: center; color: gray; font-style: italic;'> 
+    بۆ یەکەمین جار ئەم جۆرە بەرنامەیە دروستکراوە و گەشەی پێدراوە لە کوردستان و عێراق دا. 
+    هیوادارم سوودی لێوەربگرن </p> """, unsafe_allow_html=True)
     
-   st.markdown(""" <p style='text-align: center; color: gray; font-style: italic;'>  م. هەکاری جلال محمد </p> """, unsafe_allow_html=True)
+    st.markdown(""" <p style='text-align: center; color: gray; font-style: italic;'>  
+    م. هەکاری جلال محمد </p> """, unsafe_allow_html=True)
 
     st.sidebar.title("Choose Calculator")
     calculator_type = st.sidebar.radio("Select the calculator:", ("Freezing Point", "Boiling Point"))
